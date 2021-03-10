144e8ac57c2ee804e364b727eae563d940fc98c5
UserName835 <73784283+UserName835@users.noreply.github.com>
2021-03-09 18:55:09 -0600

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,19 @@
+class Ball{
+  constructor(x, y, radius) {
+      var options = {
+          'frictionAir':0.02,
+          'density':1.0
+      }
+      this.body = Bodies.circle(x, y, radius, options);
+      this.radius = radius;
+      World.add(world, this.body);
+    }
+    display(){
+      var angle = this.body.angle;
+      push();
+      translate(this.body.position.x, this.body.position.y);
+      rotate(angle);
+      ellipse(0, 0, this.radius*2, this.radius*2);
+      pop();
+    }
+}
